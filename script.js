f3b7ee9a670e1b4b3db797a860fd7781d70265b0
GustavoCMatias <gustavocmatias@gmail.com>
2022-10-22 13:16:52 -0300

    Javascript feito. Versão sem bonus pronta.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let carta_anterior = '';
+let pares_encontrados = 0;
+let jogadas = 0;
 
 let cartas = 0;
 const gifs = ['revertitparrot', 'unicornparrot', 'explodyparrot', 'fiestaparrot', 'bobrossparrot', 'metalparrot', 'tripletsparrot'];
@@ -13,7 +16,7 @@ gifsusados.sort(comparador);
 const espaco_cartas = document.querySelector(".espaco-cartas")
 
 for (let i = 0; i < gifsusados.length; i++) {
-    espaco_cartas.innerHTML += `<li class="carta" onclick="virar(this)">
+    espaco_cartas.innerHTML += `<li class="carta" onclick="virar(this, '${gifsusados[i]}')">
     <div class="frente face">
         <img src="./arquivosUteis/back.png">
     </div>
@@ -28,6 +31,38 @@ function comparador() {
     return Math.random() - 0.5;
 }
 
-function virar(carta) {
-    carta.classList.toggle("virar")
+function virar(carta, gif) {
+    jogadas++;
+    carta.classList.add("virar")
+    if (carta_anterior == '') {
+        carta_anterior = gif;
+        carta.classList.add("standby")
+
+    } else {
+        if (gif == carta_anterior) {
+
+            pares_encontrados++;
+            carta_anterior = '';
+            const carta_antiga = document.querySelector('.standby');
+            carta_antiga.classList.remove('standby');
+            if (pares_encontrados == pares) {
+                setTimeout(function () {
+                    alert(`Parabéns! Você ganhou em ${jogadas} jogadas!`);
+                }, 100)
+
+            }
+
+        } else {
+            setTimeout(desvirar, 1000, carta);
+            carta_anterior = '';
+        }
+
+    }
+}
+
+function desvirar(carta) {
+    const carta_antiga = document.querySelector('.standby');
+    carta_antiga.classList.remove('standby');
+    carta_antiga.classList.remove('virar');
+    carta.classList.remove('virar');
 }
